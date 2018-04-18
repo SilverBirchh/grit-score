8ced53ccd7a19b0fa446f92cfd298b15bb92d7a1
SilverBirchh <bradleybirch09@yahoo.co.uk>
2018-04-18 22:53:48 +0100

    Adds all grit questions

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,69 +1,67 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import { get } from '@ember/object';
+import {
+  inject as service
+} from '@ember/service';
+import {
+  get
+} from '@ember/object';
 
 export default Route.extend({
-	session: service(),
+  session: service(),
 
-	store: service(),
+  store: service(),
 
-	model() {
-		const uid = this.get('session.currentUser.uid');
-		return this.get('store')
-			.find('user', uid)
-			.catch(() => {
-				return this.get('store')
-					.createRecord('user', {
-						id: uid,
-						gender: 'male',
-						age: -1,
-						newIdeasDistractMe: -1,
-						setBacksDontDiscourage: -1,
-						changingGoals: -1,
-						hardWorker: -1,
-						difficultMaintainingFocus: -1,
-						finishWhatIStart: -1,
-						interestsChange: -1,
-						diligent: -1,
-						obsessedButGaveUp: -1,
-						overcomeSetBacks: -1,
-						gritScore: -1,
-						hoursWorkedWeek: -1,
-						storyPointsCompletedPerSprint: -1,
-					})
-					.save();
-			});
-	},
+  model() {
+    const uid = this.get('session.currentUser.uid');
+    return this.get('store')
+      .find('user', uid)
+      .catch(() => {
+        return this.get('store')
+          .createRecord('user', {
+            id: uid,
+            gender: 'male',
+            age: 1,
+            newIdeasDistractMe: 1,
+            setBacksDontDiscourage: 1,
+            changingGoals: 1,
+            hardWorker: 1,
+            difficultMaintainingFocus: 1,
+            finishWhatIStart: 1,
+            interestsChange: 1,
+            diligent: 1,
+            obsessedButGaveUp: 1,
+            overcomeSetBacks: 1,
+            gritScore: 1,
+            hoursWorkedWeek: 1,
+            storyPointsCompletedPerSprint: 1,
+          })
+          .save();
+      });
+  },
 
-	actions: {
-		save(prop) {
-			const id = this.get('session.currentUser.uid');
-			const propValue = get(this.modelFor('question'), prop);
+  actions: {
+    save(prop) {
+      const id = this.get('session.currentUser.uid');
+      const propValue = get(this.modelFor('question'), prop);
 
-			this.get('store')
-				.findRecord('user', id)
-				.then(function(user) {
-					user.set(prop, propValue);
-					user.save();
-				});
-		},
+      this.get('store')
+        .findRecord('user', id)
+        .then(function(user) {
+          user.set(prop, propValue);
+          user.save();
+        });
+    },
 
-		next() {
-			let route = Number(
-				this.router.currentRouteName.substring(
-					this.router.currentRouteName.length - 1
-				)
-			);
-			this.transitionTo(`question.${++route}`);
-		},
+    next() {
+      let route = Number(this.router.currentRouteName.substring(this.router.currentRouteName.indexOf('.') +
+        1));
+      this.transitionTo(`question.${++route}`);
+    },
 
-		previous() {
-			let route = Number(
-				this.router.currentRouteName.substring(
-					this.router.currentRouteName.length - 1
-				)
-			);
-			this.transitionTo(`question.${--route}`);
-		},
-	},
+    previous() {
+      let route = Number(this.router.currentRouteName.substring(this.router.currentRouteName.indexOf('.') +
+        1));
+      this.transitionTo(`question.${--route}`);
+    },
+  },
 });
--- app/routes/question/1.js
+++ app/routes/question/1.js
@@ -1,12 +1,5 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import { get } from '@ember/object';
+
 export default Route.extend({
-	session: service(),
 
-	store: service(),
-
-	model() {
-		return this.modelFor('question');
-	},
 });
