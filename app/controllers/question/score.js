ab6d3e16c6c7bf5554b6626937620f8ca98e73ae
SilverBirchh <bradleybirch09@yahoo.co.uk>
2018-04-19 20:02:50 +0100

    Show different colors

--- app/controllers/question/score.js
+++ app/controllers/question/score.js
@@ -28,4 +28,24 @@ export default Controller.extend({
     }
 
 	}),
+
+  color: computed('model.gritScore', function() {
+    const score = this.get('model.gritScore');
+
+    if (score > 0 && score <=1) {
+      return 'red'
+    }
+    else if (score > 1 && score <=2) {
+      return 'orange'
+    }
+    else if (score > 2 && score <=3) {
+      return 'purple'
+    }
+    else if (score > 3 && score <=4) {
+      return 'blue'
+    }
+    else if (score > 4 && score <=5) {
+      return 'green'
+    }
+  }),
 });
